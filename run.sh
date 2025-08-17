0cddc9a538df190975a5a4354f0f031e30b1b9d4
Gabo-Mac <gabgonlo05@gmail.com>
2025-08-17 13:02:10 -0600

    FIX: run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 pg_restore -U your_user -d database_name -v "Construction/TaskManager/database/task_manager_db.dump"
 
-pyinstaller --name "Tapp" --onefile --windowed --icon=MyIcon.icns --add-data "resources:resources" Construction/TaskManager/src/main.py
+cd Construction/TaskManager/src
 
-cp dist/Tapp.app /Applications/
+pyinstaller --name "Tapp" --onefile --windowed --icon=MyIcon.icns --add-data "resources:resources" main.py
+
+ditto dist/Tapp.app /Applications/Tapp.app
+
+cd .. && cd .. && cd ..
